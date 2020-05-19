079e4afe6e82a6d261e1fa2ebc78c8dc7136e312
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-05-19 11:40:23 -0700

    Add Compression, Style Fixes

--- client/src/components/DropdownContainer.jsx
+++ client/src/components/DropdownContainer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 /* eslint-disable radix */
 import styled from 'styled-components';
 import React from 'react';
@@ -86,36 +87,37 @@ const TimeDropdown = (props) => (
   <SelectWrapper>
     <LeftSvgSpan>
       <SvgLight xmlns='http://www.w3.org/2000/svg'>
-      <path d='M9 17A8 8 0 1 1 9 1a8 8 0 0 1 0 16zM9 2C5.14 2 2 5.14 2 9s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm3.223 10.098a.998.998 0 0 1-.588-.192L8 9.256V5a1 1 0 0 1 2 0v3.24l2.812 2.05a1 1 0 0 1-.59 1.808z'></path>
+        <path d='M9 17A8 8 0 1 1 9 1a8 8 0 0 1 0 16zM9 2C5.14 2 2 5.14 2 9s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm3.223 10.098a.998.998 0 0 1-.588-.192L8 9.256V5a1 1 0 0 1 2 0v3.24l2.812 2.05a1 1 0 0 1-.59 1.808z'></path>
       </SvgLight>
     </LeftSvgSpan>
     <RightSvgSpan>
       <SvgDark xmlns='http://www.w3.org/2000/svg'>
-      <path d='M8 10.5a1 1 0 0 1-.7-.29l-3.06-3a1 1 0 1 1 1.41-1.42L8 8.1l2.35-2.31a1 1 0 0 1 1.41 1.42l-3.06 3a1 1 0 0 1-.7.29z'></path>
+        <path d='M8 10.5a1 1 0 0 1-.7-.29l-3.06-3a1 1 0 1 1 1.41-1.42L8 8.1l2.35-2.31a1 1 0 0 1 1.41 1.42l-3.06 3a1 1 0 0 1-.7.29z'></path>
       </SvgDark>
     </RightSvgSpan>
     <SelectBox>
-    {props.timeslots[calendarHelpers.weekdayFromId(props.selectedDateId)].map((slot) => <option value={slot}>
-      {parseTimeslot(slot)}</option>)}
+      {props.timeslots[calendarHelpers.weekdayFromId(props.selectedDateId)]
+        .map((slot) => <option value={slot}>
+          {parseTimeslot(slot)}</option>)}
     </SelectBox>
   </SelectWrapper>
 );
 const SizeDropdown = () => (
   <SelectWrapper>
-     <LeftSvgSpan>
+    <LeftSvgSpan>
       <SvgLight xmlns='http://www.w3.org/2000/svg'>
-      <path d='M7.904 9.43l-2.098 4.697a.9.9 0 0 1-1.612 0L2.096 9.43a.902.902 0 0 1 .806-1.305h4.196c.67 0 1.105.705.806 1.305zM5 7.375a2 2 0 1 1 0-4 2 2 0 0 1 0 4z'></path>
-      <path d='M15.904 9.43l-2.098 4.697a.89.89 0 0 1-.806.498.89.89 0 0 1-.806-.498L10.096 9.43a.902.902 0 0 1 .806-1.305h4.195c.67 0 1.106.705.807 1.305zM13 7.375a2 2 0 1 1 0-4 2 2 0 0 1 0 4z' opacity=".502"></path>
+        <path d='M7.904 9.43l-2.098 4.697a.9.9 0 0 1-1.612 0L2.096 9.43a.902.902 0 0 1 .806-1.305h4.196c.67 0 1.105.705.806 1.305zM5 7.375a2 2 0 1 1 0-4 2 2 0 0 1 0 4z'></path>
+        <path d='M15.904 9.43l-2.098 4.697a.89.89 0 0 1-.806.498.89.89 0 0 1-.806-.498L10.096 9.43a.902.902 0 0 1 .806-1.305h4.195c.67 0 1.106.705.807 1.305zM13 7.375a2 2 0 1 1 0-4 2 2 0 0 1 0 4z' opacity=".502"></path>
       </SvgLight>
     </LeftSvgSpan>
     <RightSvgSpan>
       <SvgDark xmlns='http://www.w3.org/2000/svg'>
-      <path d='M8 10.5a1 1 0 0 1-.7-.29l-3.06-3a1 1 0 1 1 1.41-1.42L8 8.1l2.35-2.31a1 1 0 0 1 1.41 1.42l-3.06 3a1 1 0 0 1-.7.29z'></path>
+        <path d='M8 10.5a1 1 0 0 1-.7-.29l-3.06-3a1 1 0 1 1 1.41-1.42L8 8.1l2.35-2.31a1 1 0 0 1 1.41 1.42l-3.06 3a1 1 0 0 1-.7.29z'></path>
       </SvgDark>
     </RightSvgSpan>
     <SelectBox defaultValue='2'>
-    <option value='1'>1 person</option>
-    {peopleArray(20).map((number) => <option value={number}>{number} people</option>)}
+      <option value='1'>1 person</option>
+      {peopleArray(20).map((number) => <option value={number}>{number} people</option>)}
     </SelectBox>
   </SelectWrapper>
 );
@@ -125,8 +127,8 @@ const DropdownBox = styled.div`
 `;
 const DropdownContainer = (props) => (
   <DropdownBox>
-    <TimeDropdown timeslots={props.timeslots} selectedDateId={props.selectedDateId}/>
-    <SizeDropdown/>
+    <TimeDropdown timeslots={props.timeslots} selectedDateId={props.selectedDateId} />
+    <SizeDropdown />
   </DropdownBox>
 );
 export default DropdownContainer;
